85feada05be5934a1fcb916157db8003cdc7134c
sparrowsl <benthorpe.dev@gmail.com>
2024-02-21 13:18:52 +0000

    feat: add home handler function

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+// Write a home handler function which writes a byte slice as the response body
+func home(writer http.ResponseWriter, req *http.Request) {
+	writer.Write([]byte("Hello world from snippetbox\n"))
+}
 
 func main() {
-	fmt.Println("Hello World!!")
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", home)
+
+	log.Print("Starting server on port :5000")
+	err := http.ListenAndServe(":5000", mux)
+	log.Fatal(err)
 }
