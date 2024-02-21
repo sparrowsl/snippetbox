6eed8bdd37d359244f49b7a515e4dd5ad9f23ed0
sparrowsl <benthorpe.dev@gmail.com>
2024-02-21 13:30:51 +0000

    add more routes; view and create snippet

--- main.go
+++ main.go
@@ -7,13 +7,24 @@ import (
 
 // Write a home handler function which writes a byte slice as the response body
 func home(writer http.ResponseWriter, req *http.Request) {
-	writer.Write([]byte("Hello world from snippetbox\n"))
+	writer.Write([]byte("Hello world from snippetbox"))
+}
+
+// A handler to create new snippet
+func createSnippet(writer http.ResponseWriter, req *http.Request) {
+	writer.Write([]byte("Create new snippet!"))
+}
+
+// A handler to view a specific snippet
+func viewSnippet(writer http.ResponseWriter, req *http.Request) {
+	writer.Write([]byte("Display a specific snippet"))
 }
 
 func main() {
 	mux := http.NewServeMux()
-
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", viewSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 
 	log.Print("Starting server on port :5000")
 	err := http.ListenAndServe(":5000", mux)
