f32e7b63cc71d2422195c55dc6d569bf5a87fcde
sparrowsl <benthorpe.dev@gmail.com>
2024-02-21 15:04:07 +0000

    add allowed headers in create snippet handler

--- main.go
+++ main.go
@@ -19,6 +19,8 @@ func home(writer http.ResponseWriter, req *http.Request) {
 // A handler to create new snippet
 func createSnippet(writer http.ResponseWriter, req *http.Request) {
 	if req.Method != "POST" {
+		// Set supported format for URL - Allowed methods; POST
+		writer.Header().Add("Allow", "POST")
 		writer.WriteHeader(http.StatusMethodNotAllowed)
 		writer.Write([]byte("Method Not Allowed"))
 		return
