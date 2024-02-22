4e53693c0c5086e18cf5ce2ff837e7e96f93c5b7
sparrowsl <benthorpe.dev@gmail.com>
2024-02-22 15:42:05 +0000

    feat: connect to our MySQL database using flag

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -14,10 +17,17 @@ type application struct {
 
 func main() {
 	address := flag.String("addr", ":5000", "HTTP Network Address")
+	dsn := flag.String("dsn", "username:password@/database", "MYSQL Data Source Name")
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer db.Close()
 
 	app := &application{
 		errorLog: errorLog,
@@ -31,6 +41,20 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on port %s", *address)
-	err := server.ListenAndServe()
+	err = server.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn+"?parseTime=true")
+	if err != nil {
+		return nil, err
+	}
+
+	// Ping DB to check if it exists and setup correctly
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
