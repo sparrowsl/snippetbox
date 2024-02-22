219b7214a2b1d386e68b9efdf017dbacb70ada5e
sparrowsl <benthorpe.dev@gmail.com>
2024-02-22 11:34:47 +0000

    feat: add CLI flags for dynamic port (network address)

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	address := flag.String("addr", ":5000", "HTTP Network Address")
+	flag.Parse()
+
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
@@ -15,7 +19,7 @@ func main() {
 	mux.HandleFunc("/snippet/view", viewSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
-	log.Print("Starting server on port :5000")
-	err := http.ListenAndServe(":5000", mux)
+	log.Printf("Starting server on port %s", *address)
+	err := http.ListenAndServe(*address, mux)
 	log.Fatal(err)
 }
