a4e106862cd4e8cc135f5d82a96ea143982da338
sparrowsl <benthorpe.dev@gmail.com>
2024-02-23 09:01:54 +0000

    refactor: add an in-memory cache to load templates for reuse

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,9 +1,44 @@
 package main
 
-import "github.com/sparrowsl/snippetbox/internal/models"
+import (
+	"html/template"
+	"path/filepath"
+
+	"github.com/sparrowsl/snippetbox/internal/models"
+)
 
 // Acts as a holding structure for any dynamic data to pass in our templates
 type TemplateData struct {
 	Snippet  *models.Snippet
 	Snippets []*models.Snippet
+}
+
+func newTemplateCache() (map[string]*template.Template, error) {
+	cache := map[string]*template.Template{}
+
+	// Match all files in the filepath
+	pages, err := filepath.Glob("./ui/html/pages/*.html")
+	if err != nil {
+		return nil, err
+	}
+
+	for _, page := range pages {
+		// Return the name of the file from filepath
+		name := filepath.Base(page)
+
+		files := []string{
+			"./ui/html/base.html",
+			"./ui/html/partials/nav.html",
+			page,
+		}
+
+		temp, err := template.ParseFiles(files...)
+		if err != nil {
+			return nil, err
+		}
+
+		cache[name] = temp
+	}
+
+	return cache, nil
 }
