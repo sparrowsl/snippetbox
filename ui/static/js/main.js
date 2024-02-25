bbf94576d34fce7fab8b9ebe659743bd5ccb2105
sparrowsl <benthorpe.dev@gmail.com>
2024-02-25 06:58:41 +0000

    refactor: format time with custom js

--- ui/static/js/main.js
+++ ui/static/js/main.js
@@ -1,14 +1,19 @@
 "use strict"
 
+document.getElementById("year").outerHTML = new Date().getFullYear();
 const navLinks = document.querySelectorAll("nav a");
+const datesToFormat = document.querySelectorAll("[data-custom-date]");
 
-for (let i = 0; i < navLinks.length; i++) {
-  const link = navLinks[i]
-
+for (const link of navLinks) {
   if (link.getAttribute('href') === window.location.pathname) {
     link.classList.add("live");
     break;
   }
 }
 
-function formatDate() { }
+for (const element of datesToFormat) {
+  const newDate = new Date(element.textContent)
+  const formattedDate = new Intl.DateTimeFormat("en-US", { dateStyle: "long", timeStyle: "short" }).format(newDate)
+
+  element.textContent = formattedDate
+}
