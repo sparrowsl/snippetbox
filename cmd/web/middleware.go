702916504342be157ec7ba6996e08bf1819c8ba1
sparrowsl <benthorpe.dev@gmail.com>
2024-02-26 13:05:06 +0000

    feat: add CSP headers for security

---
+++ cmd/web/middleware.go
@@ -0,0 +1,16 @@
+package main
+
+import "net/http"
+
+func secureHeaders(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		writer.Header().Set("Content-Security-Policy", "default-src 'self'; style-src 'self' fonts.googleapis.com; font-src fonts.gstatic.com")
+		writer.Header().Set("Referrer-Policy", "origin-when-cross-origin")
+		writer.Header().Set("X-Content-Type-Options", "nosniff")
+		writer.Header().Set("X-Frame-Options", "deny")
+		writer.Header().Set("X-XSS-Protection", "0")
+
+		// Move to next handler if successful
+		next.ServeHTTP(writer, request)
+	})
+}
